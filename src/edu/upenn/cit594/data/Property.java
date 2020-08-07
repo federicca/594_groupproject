4120a056233b61e8bacf044f87d1ce3c9479a3c3
Federica Pelzel <fedepelzel@gmail.com>
2020-08-06 21:34:00 -0400

    create data obejcts

--- src/edu/upenn/cit594/data/Property.java
+++ src/edu/upenn/cit594/data/Property.java
@@ -1,4 +1,47 @@
 package edu.upenn.cit594.data;
 
 public class Property {
+    private int zipCode;
+    private int livableArea;
+    private int marketValue;
+    private int buildingCode;
+
+    public Property(int zipCode, int livableArea, int marketValue, int buildingCode){
+        this.livableArea = livableArea;
+        this.zipCode = zipCode;
+        this.marketValue = marketValue;
+        this.buildingCode = buildingCode;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getLiveableArea() {
+        return livableArea;
+    }
+
+    public void setLiveableArea(int livableArea) {
+        this.livableArea = livableArea;
+    }
+
+    public int getMarketValue() {
+        return marketValue;
+    }
+
+    public void setMarketValue(int marketValue) {
+        this.marketValue = marketValue;
+    }
+
+    public int getBuildingCode() {
+        return buildingCode;
+    }
+
+    public void setBuildingCode(int buildingCode) {
+        this.buildingCode = buildingCode;
+    }
 }
--- src/edu/upenn/cit594/data/ZipCode.java
+++ src/edu/upenn/cit594/data/ZipCode.java
@@ -1,4 +1,50 @@
 package edu.upenn.cit594.data;
 
+import java.util.ArrayList;
+
 public class ZipCode {
+    private int code;
+    private int population;
+    private ArrayList<Property> properties = new ArrayList<>();
+    private ArrayList<ParkingViolation> parkingViolations = new ArrayList<>();
+
+    public ZipCode(int code, int population,
+                   ArrayList<Property> properties, ArrayList<ParkingViolation> parkingViolations){
+        this.code = code;
+        this.population = population;
+        this.properties = properties;
+        this.parkingViolations = parkingViolations;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public ArrayList<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ArrayList<Property> properties) {
+        this.properties = properties;
+    }
+
+    public ArrayList<ParkingViolation> getParkingViolations() {
+        return parkingViolations;
+    }
+
+    public void setParkingViolations(ArrayList<ParkingViolation> parkingViolations) {
+        this.parkingViolations = parkingViolations;
+    }
 }
