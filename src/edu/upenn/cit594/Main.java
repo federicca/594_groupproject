bf1432df06234f4f621cada81bf4d6a251862b66
Federica Pelzel <fedepelzel@gmail.com>
2020-08-07 20:06:00 -0400

    created user interaction and updated main

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -4,6 +4,7 @@ import edu.upenn.cit594.data.ZipCode;
 import edu.upenn.cit594.datamanagement.*;
 import edu.upenn.cit594.processor.PopulationReader;
 import edu.upenn.cit594.processor.PropertiesReader;
+import edu.upenn.cit594.ui.UserInteraction;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -27,30 +28,20 @@ public class Main {
         TreeMap<Integer, ZipCode> zipCodeTreeMap;
         //Call function that populates the TreeMap and assigns the population value
         zipCodeTreeMap = pr.processPop("src/sample_files/population.txt");
-        // 1. Total Population for All ZIP Codes
-        TotalPopulation pop = new TotalPopulation();
-        int totalPop = pop.getTotalPop(zipCodeTreeMap);
-        System.out.println("1. Total Population for all Zip Codes: " + totalPop);
         // Create property reader and add properties to ZipCode TreeMap properties ArrayList
         PropertiesReader props = new PropertiesReader();
         props.processProperties("src/sample_files/properties_small.csv", zipCodeTreeMap);
-        // 3. Average Market Value
-        AverageMarketValue avgMarketValue = new AverageMarketValue();
-        System.out.println("3. Average market value for ZipCode: " + avgMarketValue.averageMarketValue(19143,zipCodeTreeMap));
-        // 4. Average Livable Area - Use Strategy Design
-        Context context = new Context(new AverageLiveableArea());
-        System.out.println("4. Average Livable Area: " + context.executeStrategy(19143,zipCodeTreeMap));
-        // 5. Total Residential Market Value Per Capita
-        TotalValuePC valuePC = new TotalValuePC();
-        System.out.println("5. Total Residential Market Value Per Capita: " +  valuePC.getTotalValuePC(19143,zipCodeTreeMap));
+        // Start User Interaction
+        UserInteraction ui = new UserInteraction();
+        ui.initUI(zipCodeTreeMap);
 
-        //Print for testing
+        /*Print for testing
         for (Map.Entry<Integer, ZipCode> entry : zipCodeTreeMap.entrySet()) {
             if(entry.getValue().getProperties() != null) {
                 System.out.println("ZipCode: " + entry.getKey().toString() + " Population: " + entry.getValue().getPopulation() + " Number of Properties: " + entry.getValue().getProperties().size());
             }
         }
-
+        */
 
 
     }
--- src/edu/upenn/cit594/ui/UserInteraction.java
+++ src/edu/upenn/cit594/ui/UserInteraction.java
@@ -1,4 +1,37 @@
 package edu.upenn.cit594.ui;
 
+import edu.upenn.cit594.data.ZipCode;
+import edu.upenn.cit594.datamanagement.*;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class UserInteraction {
+    public void initUI (TreeMap<Integer, ZipCode> zipCodeTreeMap){
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Welcome User!\nEnter and option between 1-6, or 0 to Exit program\n");
+        int userOpt;
+        while ((userOpt = sc.nextInt()) != 0){
+            if(userOpt == 1){
+                TotalPopulation pop = new TotalPopulation();
+                int totalPop = pop.getTotalPop(zipCodeTreeMap);
+                System.out.println("Total Population for all Zip Codes: " + totalPop);
+            }else if (userOpt==2){
+
+            } else if (userOpt==3){
+                AverageMarketValue avgMarketValue = new AverageMarketValue();
+                System.out.println("Average market value for ZipCode: " + avgMarketValue.averageMarketValue(19143,zipCodeTreeMap));
+            }else if (userOpt==4){
+                Context context = new Context(new AverageLiveableArea());
+                System.out.println("Average Livable Area: " + context.executeStrategy(19143,zipCodeTreeMap));
+            }else if (userOpt==5){
+                TotalValuePC valuePC = new TotalValuePC();
+                System.out.println("Total Residential Market Value Per Capita: " +  valuePC.getTotalValuePC(19143,zipCodeTreeMap));
+            }else if (userOpt==6){
+
+            }
+
+            System.out.println("\nEnter and option between 1-6, or 0 to Exit program");
+        }
+    }
 }
