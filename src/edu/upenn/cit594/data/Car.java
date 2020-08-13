d5f58e0dee6df6a1f7c92d48f75703d9b2993364
sbivins94 <bivins.sean7@gmail.com>
2020-08-13 12:19:03 -0500

    Create Car.java

---
+++ src/edu/upenn/cit594/data/Car.java
@@ -0,0 +1,41 @@
+package edu.upenn.cit594.data;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Car {
+    
+    private int id;
+    private String state; 
+    private HashSet<ParkingViolation> violations; 
+    // ^HS to take advantage of efficiency w/ adding nodes and contains()
+    
+    public Car(int id, String state) {
+        this.id = id;
+        this.state = state;
+        violations = new HashSet<>();
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public HashSet<ParkingViolation> getViolations() {
+        return violations;
+    }
+
+    /**
+     * Add a parking violation to this car's list of violations
+     * @param pv
+     */
+    public void addViolation(ParkingViolation pv) {
+        if (pv == null) throw new IllegalArgumentException();
+        if (violations.contains(pv)) return; // don't add duplicate violations
+        violations.add(pv);
+    }
+
+}
