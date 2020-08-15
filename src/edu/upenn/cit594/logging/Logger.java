36a5572022d5811c2cf3aca181fe54b3d9ef70a5
sbivins94 <bivins.sean7@gmail.com>
2020-08-15 15:19:21 -0500

    Logger.java_v01

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,4 +1,34 @@
 package edu.upenn.cit594.logging;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Logger {
+    
+    private PrintWriter out;
+    public static String filename;
+    
+    private Logger() {}
+    
+    private static Logger instance = new Logger();
+    
+    public static Logger getInstance() {
+        return instance;
+    }
+    
+    public void createWriter() {
+        try {
+            FileWriter fw = new FileWriter(filename, true);
+            out = new PrintWriter(fw);
+        } catch (IOException e) {
+            System.out.println("Unable to create file writer");
+            e.printStackTrace();
+        }
+    }
+    
+    public void log(String msg) {
+        out.println(msg);
+        out.flush();
+    }
 }
