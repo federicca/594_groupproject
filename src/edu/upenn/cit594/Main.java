f28151c1e0ae9c488a89a07f881b75ebf0532f3f
Federica Pelzel <fedepelzel@gmail.com>
2020-08-16 11:43:49 -0400

    Fixed index out of bounds error that was happening after changes

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,19 +1,16 @@
 package edu.upenn.cit594;
 
-import edu.upenn.cit594.data.ZipCode;
-import edu.upenn.cit594.datamanagement.PopulationReader;
-import edu.upenn.cit594.datamanagement.PropertiesReader;
 import edu.upenn.cit594.processor.Processor;
 import edu.upenn.cit594.ui.UserInteraction;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         if(args.length != 5) {
             System.out.println("Error in runtime arguments.");
+            return;
         }
         String violationsFormat = args[0];
         String violationsFile = args[1];
